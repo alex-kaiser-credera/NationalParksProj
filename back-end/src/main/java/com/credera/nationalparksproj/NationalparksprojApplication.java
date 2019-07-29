a67961b00c32e716f3ab5e564fd40bedba27a0c0
Alex Kaiser <alex.kaiser@credera.com>
2019-07-29 13:47:27 -0500

    Added Skeleton Framework

---
+++ back-end/src/main/java/com/credera/nationalparksproj/NationalparksprojApplication.java
@@ -0,0 +1,13 @@
+package com.credera.nationalparksproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalparksprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationalparksprojApplication.class, args);
+	}
+
+}
