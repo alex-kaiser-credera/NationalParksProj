05642a992e0499a53dffd72eaa759a317f864e75
Alex Kaiser <alex.kaiser@credera.com>
2019-07-30 12:31:35 -0500

    Deleted Unnecessary Node-Modules

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,7 +5,7 @@ import Home from './Home';
 import CustomerRequest from './CustomerRequest/CustomerRequest';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<CustomerRequest />, document.getElementById('root'));
+ReactDOM.render(<EmployeeQueue />, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
