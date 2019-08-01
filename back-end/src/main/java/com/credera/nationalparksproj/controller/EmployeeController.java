aea1012f2ffde20e308b0e9560f82e01c98750e2
Ryan Tanoury <ryan.tanoury@credera.com>
2019-08-01 11:51:04 -0500

    Committing database and controller stuff

--- back-end/src/main/java/com/credera/nationalparksproj/controller/EmployeeController.java
+++ back-end/src/main/java/com/credera/nationalparksproj/controller/EmployeeController.java
@@ -1,13 +1,31 @@
 package com.credera.nationalparksproj.controller;
 
 import com.credera.nationalparksproj.service.EmployeeService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EmployeeController {
 
     @Autowired
     EmployeeService employeeService;
+
+    @GetMapping(value = "/password")
+    public ResponseEntity getPassword (@RequestMapping(name = "")) {
+        return new ResponseEntity(    , HttpStatus.OK);
+    }
+
+
+    }
+
+
+
+
+    @PostMapping(value = "/")
+    public ResponseEntity
+
 
 }
