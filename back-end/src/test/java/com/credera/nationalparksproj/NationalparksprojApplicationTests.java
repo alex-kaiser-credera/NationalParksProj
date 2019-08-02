06adf6387cf5f36b3962aca74f141a0308a2c77b
Ryan Tanoury <ryan.tanoury@credera.com>
2019-08-02 09:25:58 -0500

    PULL REQUEST!!!

--- back-end/src/test/java/com/credera/nationalparksproj/NationalparksprojApplicationTests.java
+++ back-end/src/test/java/com/credera/nationalparksproj/NationalparksprojApplicationTests.java
@@ -1,16 +1,34 @@
 package com.credera.nationalparksproj;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class NationalparksprojApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
+
 	@Test
-	public void contextLoads() {
+	public void getPark() throws Exception{
+		ObjectMapper objectMapper = new ObjectMapper();
+		mockMvc.perform(get("/getPark/?id=5"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.name").value("Big Bend"));
+
 	}
 
 }
