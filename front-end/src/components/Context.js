105819c0d87469adabd0a289d55cd96d32b2a1d7
drich0687 <d.richards0687@gmail.com>
2019-08-02 15:42:35 -0500

    Day-5 stufff

--- front-end/src/components/Context.js
+++ front-end/src/components/Context.js
@@ -1,5 +1,6 @@
 import React, {createContext, useContext, useReducer} from 'react';
 
+
 export const StateContext = createContext();
 
 const initialRequest = {
@@ -34,6 +35,26 @@ const reducer = (state, action) => {
 export const StateProvider = ({children}) => (
   <StateContext.Provider value={useReducer(reducer, initialRequest)}>
     {children}
+    {/* useEffect(() => {
+      setLoading(true);
+      // ES8 Async/Await https://alligator.io/js/async-functions/
+      async function fetchAll() {
+        const result = await axios("https://www.reddit.com/r/all.json");
+
+        // You know why this is important ;)
+        result.data.data.children = result.data.data.children.filter(
+          ele => !ele.data.over_18
+        );
+
+        setData(d => ({
+          ...d,
+          all: result.data.data
+        }));
+
+        setLoading(false);
+      }
+      fetchAll();
+    }, []); //["https://www.reddit.com/r/all.json"]); */}
   </StateContext.Provider>
 );
 
