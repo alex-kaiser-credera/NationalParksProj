784414317bb164be7e8a0739adc26f8c3562e26b
drich0687 <d.richards0687@gmail.com>
2019-08-06 09:47:08 -0500

    commit changes to employeequeue

--- front-end/src/pages/EmployeeQueue.js
+++ front-end/src/pages/EmployeeQueue.js
@@ -9,7 +9,7 @@ import Paper from '@material-ui/core/Paper';
 import { InputLabel, FormHelperText } from '@material-ui/core';
 import { Select, FormControl, MenuItem, Input} from '@material-ui/core';
 import axios from 'axios';
-import { Login } from "../SignIn"
+import LogIn from "../SignIn";
 
 const StyledTableCell = withStyles(theme => ({
   head: {
@@ -40,57 +40,10 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-// const FilterList = () => {
-//   const [dropdown, setDropdown] = React.useState('');
-//   const handleChange = (event) => {
-//     setDropdown(event.target.value);
-//   }
-    
-//   return (
-//     <div>
-//       <FormControl>
-//         <InputLabel htmlfor='filter'>Filter</InputLabel>
-//         <Select 
-//           value= {dropdown}
-//           onChange= {handleChange}
-//           inputProps={{
-//               name: 'filter',
-//               id: 'filter',
-//           }}>
-//             <MenuItem value={1}>All</MenuItem>
-//             <MenuItem value={2}>In progress</MenuItem>
-//             <MenuItem value={3}>Completed</MenuItem>
-//         </Select>
-//         <FormHelperText>Please select a filter</FormHelperText>
-//       </FormControl>
-//     </div>
-//   )
-// }
-
-const Confirmation = () => {
-  const [textField, setTextField] = React.useState('');
-
-  const handleChange = (event) => {
-      setTextField(event.target.value);
-  }
-    return(
-        <div>
-        <FormControl>
-          <InputLabel htmlfor='confirmation'>Confirmation Number</InputLabel>
-          <Input 
-            id='description'
-            value= {textField}
-            onChange= {handleChange}
-          />
-          <FormHelperText>Please input Confirmation Number</FormHelperText>
-        </FormControl>
-        </div>
-    )
-}
-
 function CustomizedTables() {
   const classes = useStyles();
-  // const [{ parkLocation }] = Login([]);
+  // const [{ parkLocation }] = LogIn().park;
+
   const parkLocation = 43;
   const [result, setResult] = React.useState([]);
   const [request, setRequest] = React.useState([]);
@@ -106,49 +59,49 @@ function CustomizedTables() {
 
   useEffect(() => {
     async function fetchAll() {
-      // const result = await axios(`http://localhost:8080/status/view/${parkLocation}?status=All`);
+      const result = await axios(`http://localhost:8080/status/view/${parkLocation}?status=All`);
 
       // Mock response
-      const result = {
-        data: [
-          {
-            id: `123`,
-            status: `In Progress`,
-            dateCreated: `b`,
-            dateCompleted: `c`,
-            parkLocation: {
-              name: `d`,
-            },
-            requestType: `e`,
-            problemDescription: `f`,
-            email: `g`,
-          },
-          {
-            id: `124`,           
-            status: `Completed`,
-            dateCreated: `ba`,
-            dateCompleted: `ca`,
-            parkLocation: {
-              name: `da`,
-            },
-            requestType: `ea`,
-            problemDescription: `fa`,
-            email: `ga`,
-          },
-          {
-            id: `125`,
-            status: `In Progress`,
-            dateCreated: `bb`,
-            dateCompleted: `cb`,
-            parkLocation: {
-              name: `db`,
-            },
-            requestType: `eb`,
-            problemDescription: `fb`,
-            email: `gb`,
-          },
-        ]
-      }
+      // const result = {
+      //   data: [
+      //     {
+      //       id: `123`,
+      //       status: `In Progress`,
+      //       dateCreated: `b`,
+      //       dateCompleted: `c`,
+      //       parkLocation: {
+      //         name: `d`,
+      //       },
+      //       requestType: `e`,
+      //       problemDescription: `f`,
+      //       email: `g`,
+      //     },
+      //     {
+      //       id: `124`,           
+      //       status: `Completed`,
+      //       dateCreated: `ba`,
+      //       dateCompleted: `ca`,
+      //       parkLocation: {
+      //         name: `da`,
+      //       },
+      //       requestType: `ea`,
+      //       problemDescription: `fa`,
+      //       email: `ga`,
+      //     },
+      //     {
+      //       id: `125`,
+      //       status: `In Progress`,
+      //       dateCreated: `bb`,
+      //       dateCompleted: `cb`,
+      //       parkLocation: {
+      //         name: `db`,
+      //       },
+      //       requestType: `eb`,
+      //       problemDescription: `fb`,
+      //       email: `gb`,
+      //     },
+      //   ]
+      // }
 
       setResult(result.data);
     }
@@ -171,8 +124,7 @@ function CustomizedTables() {
   
   // Filter by confirmation number
   if (textField !== ``) {
-    console.warn(`###`, textField);
-    filteredData = result.filter(item => item.id === textField);
+    filteredData = result.filter(item => item.id === parseInt(textField));
   }
 
   return (
@@ -211,52 +163,52 @@ function CustomizedTables() {
         </FormControl>
       </div>
 
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <StyledTableCell align="right">Confirmation Number</StyledTableCell>
-            <StyledTableCell align="right">Status</StyledTableCell>
-            <StyledTableCell align="right">Date Created</StyledTableCell>
-            <StyledTableCell align="right">Date Completed</StyledTableCell>
-            <StyledTableCell align="right">Park</StyledTableCell>
-            <StyledTableCell align="right">Request Type</StyledTableCell>
-            <StyledTableCell align="right">Description</StyledTableCell>
-            <StyledTableCell align="right">Visitor Email</StyledTableCell>
-          </TableRow>
-        </TableHead>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="right">Confirmation Number</StyledTableCell>
+              <StyledTableCell align="right">Status</StyledTableCell>
+              <StyledTableCell align="right">Date Created</StyledTableCell>
+              <StyledTableCell align="right">Date Completed</StyledTableCell>
+              <StyledTableCell align="right">Park</StyledTableCell>
+              <StyledTableCell align="right">Request Type</StyledTableCell>
+              <StyledTableCell align="right">Description</StyledTableCell>
+              <StyledTableCell align="right">Visitor Email</StyledTableCell>
+            </TableRow>
+          </TableHead>
 
-        <TableBody 
-          className={classes.labels}
-          value={request}
-          onClick={selectRequest}
-          inputProps={{
-            id: 'request.id',
-            status: 'request.status',
-            dateCreated: 'request.datecreated',
-            dateCompleted: 'request.datecompleted',
-            parkLocation: 'request.parklocation',
-            requestType: 'request.requesttype',
-            problemDescription: 'request.problemdescription',
-            email: 'request.email',
-          }}
-        >
-        {filteredData.map((e) => (
-          <StyledTableRow key={e.id}>
-            <StyledTableCell align="right">{e.id}</StyledTableCell>
-            <StyledTableCell align="right">{e.status}</StyledTableCell>
-            <StyledTableCell align="right">{e.dateCreated}</StyledTableCell>
-            <StyledTableCell align="right">{e.dateCompleted}</StyledTableCell>
-            <StyledTableCell align="right">{e.parkLocation.name}</StyledTableCell>
-            <StyledTableCell align="right">{e.requestType}</StyledTableCell>
-            <StyledTableCell align="right">{e.problemDescription}</StyledTableCell>
-            <StyledTableCell align="right">{e.email}</StyledTableCell>
-          </StyledTableRow>
-        ))}
-        </TableBody>
-      </Table>
-    </Paper>
-  </div>
+          <TableBody 
+            className={classes.labels}
+            value={request}
+            onClick={selectRequest}
+            inputProps={{
+              id: 'request.id',
+              status: 'request.status',
+              dateCreated: 'request.datecreated',
+              dateCompleted: 'request.datecompleted',
+              parkLocation: 'request.parklocation',
+              requestType: 'request.requesttype',
+              problemDescription: 'request.problemdescription',
+              email: 'request.email',
+            }}
+          >
+          {filteredData.map((e) => (
+            <StyledTableRow key={e.id}>
+              <StyledTableCell align="right">{e.id}</StyledTableCell>
+              <StyledTableCell align="right">{e.status}</StyledTableCell>
+              <StyledTableCell align="right">{e.dateCreated}</StyledTableCell>
+              <StyledTableCell align="right">{e.dateCompleted}</StyledTableCell>
+              <StyledTableCell align="right">{e.parkLocation.name}</StyledTableCell>
+              <StyledTableCell align="right">{e.requestType}</StyledTableCell>
+              <StyledTableCell align="right">{e.problemDescription}</StyledTableCell>
+              <StyledTableCell align="right">{e.email}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    </div>
   );
 }
 
