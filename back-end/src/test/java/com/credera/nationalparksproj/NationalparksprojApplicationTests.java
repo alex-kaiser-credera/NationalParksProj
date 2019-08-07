36936d098e46aa4b2f613658bb91eea55cd01c4b
Ryan Tanoury <ryan.tanoury@credera.com>
2019-08-07 11:23:11 -0500

    Front-end Status Fun!

--- back-end/src/test/java/com/credera/nationalparksproj/NationalparksprojApplicationTests.java
+++ back-end/src/test/java/com/credera/nationalparksproj/NationalparksprojApplicationTests.java
@@ -12,6 +12,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.mail.Message;
+import javax.mail.Session;
 import java.util.Arrays;
 
 import static org.hamcrest.CoreMatchers.equalTo;
